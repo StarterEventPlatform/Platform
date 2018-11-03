4a68fa998c027892eb48bc7414c258cb82ed15f9
GlebDurygin <durygin.gleb@gmail.com>
2018-11-03 18:49:46 +0400

    feature: add implementation of EventFactory.java and Maintainerfactory.java

--- platform/src/main/java/com/eventplatform/factory/EventFactory.java
+++ platform/src/main/java/com/eventplatform/factory/EventFactory.java
@@ -1,4 +1,23 @@
 package com.eventplatform.factory;
 
+import com.eventplatform.model.Event;
+import com.eventplatform.model.GeoPosition;
+import com.eventplatform.model.Maintainer;
+
+import java.util.Date;
+import java.util.List;
+
 public class EventFactory {
+
+    public static Event createEvent(Event event) {
+        return new Event(event.getId(), event.getCreationDate(), event.getName(), event.getDescription(), event.getGeoPosition(), event.getMaintainers(), event.getType(), event.getEventDate());
+    }
+
+    public static Event createEvent(int id, String name, String description, GeoPosition geoPosition, List<Maintainer> maintainers, String type, Date eventDate) {
+        return new Event(id, new Date(System.currentTimeMillis()), name, description, geoPosition, maintainers, type, eventDate);
+    }
+
+    public static Event createEvent(int id, String name, String description, GeoPosition geoPosition, String type, Date eventDate) {
+        return new Event(id, new Date(System.currentTimeMillis()), name, description, geoPosition, type, eventDate);
+    }
 }
