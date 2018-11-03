e8002e7e461c7b7c7bc2317f83528176e6b1dd2a
GlebDurygin <durygin.gleb@gmail.com>
2018-11-03 18:49:46 +0400

    feature: add implementation of EventFactory.java and Maintainerfactory.java

--- platform/src/main/java/com/eventplatform/factory/MaintainerFactory.java
+++ platform/src/main/java/com/eventplatform/factory/MaintainerFactory.java
@@ -11,10 +11,14 @@ import java.util.List;
 public class MaintainerFactory {
 
     public static Maintainer createMaintainer(Maintainer maintainer) {
-        return new Maintainer(maintainer.getId(),maintainer.getCreationDate(),maintainer.getUser(),maintainer.getName(),maintainer.getDescription(),maintainer.getEvents(),maintainer.getGeoPosition());
+        return new Maintainer(maintainer.getId(), maintainer.getCreationDate(), maintainer.getUser(), maintainer.getName(), maintainer.getDescription(), maintainer.getEvents(), maintainer.getGeoPosition());
     }
 
     public static Maintainer createMaintainer(int id, User user, String name, String description, List<Event> events, GeoPosition geoPosition) {
-        return new Maintainer(id,new Date(System.currentTimeMillis()),user,name,description,events,geoPosition);
+        return new Maintainer(id, new Date(System.currentTimeMillis()), user, name, description, events, geoPosition);
+    }
+
+    public static Maintainer createMaintainer(int id, User user, String name, String description, GeoPosition geoPosition) {
+        return new Maintainer(id, new Date(System.currentTimeMillis()), user, name, description, geoPosition);
     }
 }
