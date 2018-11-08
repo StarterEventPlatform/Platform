ef75fc8ac349c97067318a7c1c65a8613b92dd57
GlebDurygin <durygin.gleb@gmail.com>
2018-11-08 18:09:40 +0400

    add XmlParser.java, XmlParserException.java

---
+++ platform/src/main/java/com/eventplatform/exception/utils/XmlParserException.java
@@ -0,0 +1,13 @@
+package com.eventplatform.exception.utils;
+
+import java.io.IOException;
+
+public class XmlParserException extends IOException {
+    public XmlParserException() {
+        super();
+    }
+
+    public XmlParserException(String message) {
+        super(message);
+    }
+}
