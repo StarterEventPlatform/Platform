82ee93d3373955aa61b346f237b817e8aa77b21f
GlebDurygin <durygin.gleb@gmail.com>
2018-12-07 00:30:18 +0400

    feature: add reading from mongodb, refactor mustache geoposition, geopositions

--- platform/src/main/java/com/eventplatform/controller/page/GeoPositionController.java
+++ platform/src/main/java/com/eventplatform/controller/page/GeoPositionController.java
@@ -1,16 +1,16 @@
 package com.eventplatform.controller.page;
 
-import com.eventplatform.exception.controller.EmptyControllerException;
+import com.eventplatform.exception.controller.ControllerException;
 import com.eventplatform.pojo.controller.GeoPositionDataController;
 import com.eventplatform.pojo.klass.GeoPosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
 
 @Controller
 public class GeoPositionController {
@@ -19,11 +19,7 @@ public class GeoPositionController {
 
     @RequestMapping("/geopositions")
     public String handleRequest(Model model) {
-        try {
-            model.addAttribute("geopositions", geoPositionDataController.getAll());
-        } catch (EmptyControllerException e) {
-            model.addAttribute("geopositions", new ArrayList<>());
-        }
+        model.addAttribute("geopositions", geoPositionDataController.getAll());
         return "geopositions";
     }
 
@@ -31,6 +27,17 @@ public class GeoPositionController {
     public ModelAndView createGeoPosition() {
         ModelAndView modelAndView = new ModelAndView("geoposition", "geoPosition", new GeoPosition());
         return modelAndView;
+    }
+
+    @PostMapping(value = "geoposition")
+    public String geoPositionSubmit(@RequestParam(name = "latitude") float latitude,
+                                    @RequestParam(name = "longitude") float longitude) {
+        try {
+            geoPositionDataController.create(latitude, longitude);
+        } catch (ControllerException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/geopositions";
     }
 
 
