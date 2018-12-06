f6ff57a8f8f455edd7495fa3c454e522d55c4e24
GlebDurygin <durygin.gleb@gmail.com>
2018-12-06 21:55:14 +0400

    refactor: refactor after rebase on master

--- platform/src/main/java/com/eventplatform/controller/page/GeoPositionController.java
+++ platform/src/main/java/com/eventplatform/controller/page/GeoPositionController.java
@@ -1,48 +1,29 @@
 package com.eventplatform.controller.page;
 
-import com.eventplatform.exception.controller.ControllerAggregatorException;
-import com.eventplatform.pojo.controller.DataControllerAggregator;
-import com.eventplatform.pojo.controller.DataControllerConstants;
+import com.eventplatform.exception.controller.EmptyControllerException;
 import com.eventplatform.pojo.controller.GeoPositionDataController;
 import com.eventplatform.pojo.klass.GeoPosition;
-import com.eventplatform.repository.GeoPositionDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Date;
+import java.util.ArrayList;
 
 @Controller
 public class GeoPositionController {
     @Autowired
-    private DataControllerAggregator dataControllerAggregator;
-    private final GeoPositionDataRepository geoPositionDataRepository;
-
-    public GeoPositionController(GeoPositionDataRepository geoPositionDataRepository) {
-        this.geoPositionDataRepository = geoPositionDataRepository;
-    }
+    private GeoPositionDataController geoPositionDataController;
 
     @RequestMapping("/geopositions")
     public String handleRequest(Model model) {
         try {
-            GeoPositionDataController geoPositionDataController = (GeoPositionDataController) dataControllerAggregator.getByType(DataControllerConstants.GEOPOSITION_TYPE);
-
-            for (int i = 0; i < 20; i++) {
-                GeoPosition geoPosition = new GeoPosition();
-                geoPosition.setLatitude(i);
-                geoPosition.setLongitude(i+100);
-                geoPosition.setCreationDate(new Date(System.currentTimeMillis() + i));
-                geoPositionDataRepository.save(geoPosition);
-            }
-            //model.addAttribute("geopositions",geoPositionDataRepository.findAll());
-        } catch (ControllerAggregatorException e) {
-            e.printStackTrace();
+            model.addAttribute("geopositions", geoPositionDataController.getAll());
+        } catch (EmptyControllerException e) {
+            model.addAttribute("geopositions", new ArrayList<>());
         }
-        model.addAttribute("geopositions" , null);
         return "geopositions";
     }
 
--- platform/src/main/java/com/eventplatform/repository/GeoPositionDataRepository.java
+++ platform/src/main/java/com/eventplatform/repository/GeoPositionDataRepository.java
@@ -2,7 +2,9 @@ package com.eventplatform.repository;
 
 import com.eventplatform.pojo.klass.GeoPosition;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GeoPositionDataRepository extends MongoRepository<GeoPosition,Integer> {
 
 }
