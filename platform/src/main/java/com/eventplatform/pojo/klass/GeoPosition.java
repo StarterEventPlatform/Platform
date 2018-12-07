57871928826956859c51cfbd2dab2f8a9ae24fe1
GlebDurygin <durygin.gleb@gmail.com>
2018-12-07 19:27:16 +0400

    refactor: annotation Data in GeoPosition.java

--- platform/src/main/java/com/eventplatform/pojo/klass/GeoPosition.java
+++ platform/src/main/java/com/eventplatform/pojo/klass/GeoPosition.java
@@ -1,34 +1,21 @@
 package com.eventplatform.pojo.klass;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
+@Data
 @Document(collection = "GeoPosition")
 public class GeoPosition {
 
     @Id
-    @GeneratedValue
-    @Setter
-    @Getter
     private int id;
-
-    @Setter
-    @Getter
     @Field(value = "Creation_Date")
     private Date creationDate;
-    @Setter
-    @Getter
     @Field(value = "Latitude")
     private float latitude;
-    @Setter
-    @Getter
     @Field(value = "Longitude")
     private float longitude;
 
