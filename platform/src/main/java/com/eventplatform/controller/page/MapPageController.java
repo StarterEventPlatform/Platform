4433a89fd2dd7b0e3a20ecdb1a326f3c8bc04e90
Dmitry Vasilev <vasilev-d@haulmont.com>
2019-07-19 11:49:30 +0400

    add circle layout on map and remove yandexMap

--- platform/src/main/java/com/eventplatform/controller/page/MapPageController.java
+++ platform/src/main/java/com/eventplatform/controller/page/MapPageController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MapPageController {
 
-    @RequestMapping("/yandexmap")
-    public String handleYandexRequest(Model model) {
-
-        return "yandexmap";
-    }
-
     @RequestMapping("/twogismap")
     public String handle2GisRequest(Model model) {
 
